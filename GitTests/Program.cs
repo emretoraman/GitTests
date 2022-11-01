297ecb4ebb52eb545bfe7e637507a7dafbd80f78
junksnspams <junksnspams@gmail.com>
2022-11-01 05:55:58 +0300

    Forked

--- GitTests/Program.cs
+++ GitTests/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
 Console.WriteLine("Some changes on original repo");
 
 Console.WriteLine("Some other changes on original repo");
+
+Console.WriteLine("Some feature on forked repo");
